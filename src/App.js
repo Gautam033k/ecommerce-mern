a0f7337b2a9a86e9a6bfbdf0e015e6dc30df5ead
Gowtham <gowtham2001k@gmail.com>
2022-12-29 18:59:10 +0530

    level 4-  Login and register UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import CartScreen from './Screen/CartScreen';
+// import LoginScreen from './Screen/LoginScreen';
+// import RegisterScreen from './Screen/RegisterScreen';
 // import { HomeScreen } from './Screen/HomeScreen';
 // import { ProductScreen } from './Screen/ProductScreen';
-import SingleProduct from './Screen/SingleProduct';
+// import SingleProduct from './Screen/SingleProduct';
 
 function App() {
-  return <SingleProduct />;
+  return <CartScreen />;
 }
 
 export default App;
