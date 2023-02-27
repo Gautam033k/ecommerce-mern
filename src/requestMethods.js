8bf630386338a103d3d4b786081c0b931a510fb5
Gowtham <gowtham2001k@gmail.com>
2023-02-27 16:24:52 +0530

    changes with api 1

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api/';
+const BASE_URL = 'https://ecommerce-api-7t1n.onrender.com/api/';
 const TOKEN =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYWZjZWQxMzljNDdkZTA5MzcxNzllZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NDQ1NDMwNSwiZXhwIjoxNjc0NzEzNTA1fQ.6uduPxMSza12x0ONI5CfOsSbfJNvrNsX8Sjxd9nIYrg';
 
